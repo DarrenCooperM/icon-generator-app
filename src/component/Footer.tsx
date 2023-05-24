4eef31f0c946de7d8de266704a2173662704ee0d
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-05-24 12:48:59 +1200

    hamburger menu now its own component. UI updates to menu and header now sticks

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -2,12 +2,10 @@ import { PrimaryLink } from "./PrimaryLink";
 
 export function Footer() {
   return (
-    <footer>
-      <footer className="container mx-auto grid h-24 grid-cols-3 items-center text-center">
-        <PrimaryLink href="/">icons.maoriwebdev.com</PrimaryLink>
-        <PrimaryLink href="/privacy-policy">Privacy Policy</PrimaryLink>
-        <PrimaryLink href="/terms-of-service">Terms of Service</PrimaryLink>
-      </footer>
+    <footer className="mt-auto flex h-24 w-full items-center justify-center gap-20 bg-black text-white">
+      <PrimaryLink href="/">icons.maoriwebdev.com</PrimaryLink>
+      <PrimaryLink href="/privacy-policy">Privacy Policy</PrimaryLink>
+      <PrimaryLink href="/terms-of-service">Terms of Service</PrimaryLink>
     </footer>
   );
 }
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,26 +1,15 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import { useBuyCredits } from "~/hooks/useBuyCredits";
-import { api } from "~/utils/api";
-import { Button } from "./Button";
 import { PrimaryLink } from "./PrimaryLink";
-import { Spinner } from "./Spinner";
 import { useState } from "react";
 import Image from "next/image";
+import { HamburgerMenu } from "./HamburgerMenu";
 
 export function Header() {
-  const session = useSession();
-  const { buyCredits } = useBuyCredits();
-
-  const isLoggedIn = !!session.data;
-
-  const credits = api.user.getCredits.useQuery(undefined, {
-    enabled: isLoggedIn,
-  });
-
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header>
+    <header
+      className={`${!isOpen ? "sticky top-0 z-50 bg-gray-200 shadow-md" : ""}`}
+    >
       <div className="mx-auto flex h-16 items-center justify-between gap-10 border-b-2 border-gray-500 px-4 capitalize max-lg:flex max-lg:justify-between sm:flex sm:justify-between lg:flex lg:justify-around">
         <PrimaryLink href="/">
           <Image
@@ -53,122 +42,7 @@ export function Header() {
           ></div>
         </button>
 
-        {isOpen && (
-          <div
-            className="fixed left-0 top-0 z-10 h-full w-full bg-black bg-opacity-50"
-            onClick={() => setIsOpen(false)}
-          ></div>
-        )}
-
-        <div
-          className={`fixed left-0 top-0 z-20 h-full w-64 transform items-center bg-gray-300 text-black transition-transform duration-200 dark:bg-black dark:text-white ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
-          } lg:static lg:z-0 lg:flex lg:w-auto lg:transform-none lg:space-x-4 lg:bg-transparent lg:text-black`}
-        >
-          <ul
-            className={
-              isOpen
-                ? `flex flex-col gap-4 space-y-4 px-4 pt-4 lg:flex-row lg:space-y-0`
-                : `flex flex-col gap-4 space-y-4 lg:flex-row lg:space-y-0`
-            }
-          >
-            <li
-              onClick={() => setIsOpen(false)}
-              className={
-                isOpen
-                  ? `border-b-2 border-gray-800 py-2 dark:border-b-2 dark:border-white`
-                  : ""
-              }
-            >
-              <PrimaryLink href="/generate">Generate</PrimaryLink>
-            </li>
-            <li
-              onClick={() => setIsOpen(false)}
-              className={
-                isOpen
-                  ? `border-b-2 border-gray-800 py-2 dark:border-b-2 dark:border-white`
-                  : ""
-              }
-            >
-              <PrimaryLink href="/community">Community</PrimaryLink>
-            </li>
-            {isLoggedIn && (
-              <>
-                <li
-                  onClick={() => setIsOpen(false)}
-                  className={
-                    isOpen
-                      ? `border-b-2 border-gray-800 py-2 dark:border-b-2 dark:border-white`
-                      : ""
-                  }
-                >
-                  <PrimaryLink href="/collection">Collection</PrimaryLink>
-                </li>
-                {/* <li
-                  onClick={() => setIsOpen(false)}
-                  className={
-                    isOpen
-                      ? `border-b-2 border-gray-800 py-2 dark:border-b-2 dark:border-white`
-                      : ""
-                  }
-                >
-                  <PrimaryLink href="https://portfolio.maoriwebdev.com">
-                    Portfolio
-                  </PrimaryLink>
-                </li> */}
-              </>
-            )}
-          </ul>
-          <ul
-            className={
-              isOpen
-                ? `flex flex-col gap-4 space-y-4 px-4 pt-4 lg:flex-row lg:space-y-0`
-                : `flex flex-col gap-4 space-y-4 lg:flex-row lg:space-y-0`
-            }
-          >
-            {isLoggedIn && (
-              <>
-                <li onClick={() => setIsOpen(false)}>
-                  <Button
-                    onClick={() => {
-                      buyCredits().catch(console.error);
-                    }}
-                  >
-                    Buy Credits
-                  </Button>
-                </li>
-                <li onClick={() => setIsOpen(false)}>
-                  <Button
-                    variant="secondary"
-                    onClick={() => {
-                      signOut().catch(console.error);
-                    }}
-                  >
-                    Logout
-                  </Button>
-                </li>
-                <div className="flex items-center font-medium text-blue-800">
-                  Credits remaining:&nbsp;
-                  <span>{credits.data || <Spinner />}</span>
-                </div>
-              </>
-            )}
-            {!isLoggedIn && (
-              <li
-                onClick={() => setIsOpen(false)}
-                className={isOpen ? `pt-4` : ""}
-              >
-                <Button
-                  onClick={() => {
-                    signIn().catch(console.error);
-                  }}
-                >
-                  Login
-                </Button>
-              </li>
-            )}
-          </ul>
-        </div>
+        <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
     </header>
   );
