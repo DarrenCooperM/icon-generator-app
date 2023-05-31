5473f758d47f2d6edd86da1b457762c043ecb1bb
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-05-31 14:20:30 +1200

    color of text on home page clashing with background image. All fixed now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,20 +15,20 @@ function HeroBanner() {
   return (
     <>
       <section className="mb-24 mt-12 grid grid-cols-1 gap-12 rounded-lg p-20 px-8 text-center md:gap-40 lg:mt-0 lg:grid lg:grid-cols-2 lg:bg-black lg:bg-opacity-50 lg:text-left">
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-4 text-white">
           <h1 className="text-xl font-bold lg:text-2xl ">
             {sessionData && <span>Kia Ora {sessionData.user?.name}!</span>}
           </h1>
           <h1 className="text-4xl font-bold lg:text-6xl ">
             Use AI to generate logos at the click of a button!
           </h1>
-          <p className="mt-8 text-xl lg:text-2xl">
+          <p className="mt-8 text-xl font-medium lg:text-2xl">
             Don&apos;t wait for a designer to make a logo, generate one now by
             clicking the &apos;Get Started&apos; button below!
           </p>
           <PrimaryLinkButton
             href="/generate"
-            className="mt-12 font-medium xl:self-start"
+            className="mt-12 text-center font-medium md:w-3/5 md:self-start"
           >
             GET STARTED
           </PrimaryLinkButton>
