c563f622b9d2160899fd03992fbe9e1364cf2f04
Darren Cooper-Matila <darrenc@missionreadyhq.com>
2023-05-31 16:55:45 +1200

    background img clashing with UI. Fixing now.

--- src/component/GeneralInfo.tsx
+++ src/component/GeneralInfo.tsx
@@ -1,15 +1,27 @@
+import Image from "next/image";
+
+const time = "/time.jpg";
+
 export function GeneralInfo() {
   return (
     <>
-      <div className="mx-4 max-w-screen-xl sm:mx-auto ">
-        <div className="h-70 my-24 flex flex-col items-center justify-center gap-10 rounded-lg text-center md:h-64">
+      <div className="mx-4 mb-12 max-w-screen-xl sm:mx-auto md:pb-24 md:pt-12">
+        <Image
+          src={time}
+          alt="time"
+          width={100}
+          height={100}
+          className="mx-auto h-20 w-20 rounded-xl text-gray-400 dark:text-gray-600 "
+        />
+        <div className="flex flex-col items-center justify-center gap-10 rounded-lg text-center">
           <p className="mt-8 text-4xl font-medium capitalize md:text-6xl">
             Let us save your time!
           </p>
           <p className="mx-8 mb-8 text-xl md:mx-40 md:text-2xl">
-            Hiring a designer to build web assets and digital logos for your
-            site can be hard and time consuming. Describe your logos with a
-            custom prompt, and we&apos;ll generate your assets in seconds.
+            Recruiting a designer to create digital logos and web elements for
+            your website can be a daunting and lengthy process. By providing a
+            unique description of your desired logos, we can swiftly produce
+            your assets within a matter of seconds.
           </p>
         </div>
       </div>
